25c57a36e44ef2046bbde4df813dbaecd2e9c92c
robin <derrop@outlook.de>
2025-09-25 00:08:49 +0200

    Revert "Improve torch installer"
    This reverts commit f3eada449999a29b82973bcf3378d942e54b84e7.

--- install_torch.py
+++ install_torch.py
@@ -1,28 +1,29 @@
 import subprocess
 import sys
-import importlib
-
-
-def ensure_torch_module(module: str, version: str):
-    try:
-        torch = importlib.import_module(module)
-        return torch
-    except ImportError:
-        print(f"Torch module '{module}' not found, installing it now... this will take a while!")
-        subprocess.check_call([
-            sys.executable, "-m", "pip", "install",
-            f"{module}=={version}", "--index-url", "https://download.pytorch.org/whl/cu121"
-        ])
 
 
 def install_torch():
     """
     Torch is huge (~ 2,5 GB), so we download it on the fly instead of packing it into the binary
     """
-    torch = ensure_torch_module("torch", "2.5.1+cu121")
+    try:
+        import torch
+    except ImportError:
+        print("PyTorch not found, installing it now... this will take a while!")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "torch==2.5.1+cu121",
+                               "--index-url", "https://download.pytorch.org/whl/cu121"])
+        import torch
+
     print(f"Torch version: {torch.__version__}")
 
 
 def install_torchvision():
-    torchvision = ensure_torch_module("torchvision", "0.20.1+cu121")
+    try:
+        import torchvision
+    except ImportError:
+        print("PyTorchvision not found, installing it now... this will take a while!")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "torchvision==0.20.1+cu121",
+                               "--index-url", "https://download.pytorch.org/whl/cu121"])
+        import torchvision
+
     print(f"Torchvision version: {torchvision.__version__}")
